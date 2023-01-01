3bc94664c9cc8f467c109249a387573c75884375
aliromero <aliyeketaz@gmail.com>
2023-01-01 13:19:39 +0330

    romero

--- src/Drivers/Zibal.php
+++ src/Drivers/Zibal.php
@@ -42,6 +42,7 @@ class Zibal implements Bank
                 "trackId" => $params['trackId']
             ]);
         $response = json_decode($request->getBody()->getContents(), true);
+
         return $response;
 
     }
@@ -70,5 +71,94 @@ class Zibal implements Bank
             'charset: utf-8',
             'Content-Type: application/json',
         ];
+    }
+
+    protected function resultCodes($code)
+    {
+        switch ($code)
+        {
+            case 100:
+                return "با موفقیت تایید شد";
+
+            case 102:
+                return "merchant یافت نشد";
+
+            case 103:
+                return "merchant غیرفعال";
+
+            case 104:
+                return "merchant نامعتبر";
+
+            case 201:
+                return "قبلا تایید شده";
+
+            case 105:
+                return "amount بایستی بزرگتر از 1,000 ریال باشد";
+
+            case 106:
+                return "callbackUrl نامعتبر می‌باشد. (شروع با http و یا https)";
+
+            case 113:
+                return "amount مبلغ تراکنش از سقف میزان تراکنش بیشتر است.";
+
+            case 202:
+                return "سفارش پرداخت نشده یا ناموفق بوده است";
+
+            case 203:
+                return "trackId نامعتبر می‌باشد";
+
+            default:
+                return "وضعیت مشخص شده معتبر نیست";
+        }
+    }
+    protected function statusCodes($code)
+    {
+        switch ($code)
+        {
+            case -1:
+                return "در انتظار پردخت";
+
+            case -2:
+                return "خطای داخلی";
+
+            case 1:
+                return "پرداخت شده - تاییدشده";
+
+            case 2:
+                return "پرداخت شده - تاییدنشده";
+
+            case 3:
+                return "لغوشده توسط کاربر";
+
+            case 4:
+                return "‌شماره کارت نامعتبر می‌باشد";
+
+            case 5:
+                return "‌موجودی حساب کافی نمی‌باشد";
+
+            case 6:
+                return "رمز واردشده اشتباه می‌باشد";
+
+            case 7:
+                return "‌تعداد درخواست‌ها بیش از حد مجاز می‌باشد";
+
+            case 8:
+                return "‌تعداد پرداخت اینترنتی روزانه بیش از حد مجاز می‌باشد";
+
+            case 9:
+                return "مبلغ پرداخت اینترنتی روزانه بیش از حد مجاز می‌باشد";
+
+            case 10:
+                return "‌صادرکننده‌ی کارت نامعتبر می‌باشد";
+
+            case 11:
+                return "خطای سوییچ";
+
+            case 12:
+                return "کارت قابل دسترسی نمی‌باشد";
+
+            default:
+                return "وضعیت مشخص شده معتبر نیست";
+        }
     }
 }
